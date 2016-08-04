5b9467e2717927ad23ebe4fd691d74d9c6451228
Hyun Ahn <rationality6@gmail.com>
2016-08-04 15:48:47 -0700

    Logic complete.

--- team.rb
+++ team.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-def randomiz(team_array, how_many, _method)
+def randomiz(team_array, how_many, method)
     team_array_rand = team_array.shuffle
     array_result = []
     team_count = 1
+
+    how_many = team_array.length / how_many if method
+
     slice = team_array_rand.each_slice(how_many).to_a
     for team in slice
         array_result << { team_count => team }
@@ -13,17 +16,17 @@ def randomiz(team_array, how_many, _method)
     array_result
 end
 
-# print randomiz([1, 2, 3, 4, 5, 6], 2)
+# print randomiz([1, 2, 3, 4, 5, 6], 3, true)
 
 get '/' do
     erb :index
 end
 
 post '/' do
-    puts params
-    @people = params[:names].split(',')
+    $get_people = params[:names].to_s
+    @people = $get_people.split(',')
     @method = params[:method] # radio button
     @how_many = params[:how_many].to_i # teams or people based on method
-    $randomiz = randomiz(@people, @how_many, @method) # 2 will be the count split how_many
+    $randomiz = randomiz($people, @how_many, @method) # 2 will be the count split how_many
     redirect back
 end
